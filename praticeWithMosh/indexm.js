3fa5b330413df5f11e6839066b5d90ab17c90223
Henok-eng <66444157+Henok-eng@users.noreply.github.com>
2022-05-04 11:09:37 -0500

    final24

---
+++ praticeWithMosh/indexm.js
@@ -0,0 +1,31 @@
+//console.log('Hello world');
+let name='Tewelde';
+
+let lastname='Teweldebrhan';
+// constants
+let interestrate=1;
+let person={
+    name: 'Henok', 
+    age: 30
+};
+person.name='John';
+person['name']=  'Heni';
+console.log(person);
+let selectedColors= ['blue', 'white', 'purple'];
+console.log (selectedColors);
+function greet(name){
+    console.log('hello '+name);
+}
+greet('john');
+function square(number){
+    return number*number;
+}
+
+console.log(square(2));
+
+
+
+//console.log(interestrate);
+let fname='Henok';
+let age=30;
+let isboolean= true;
